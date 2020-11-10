dc2cd968ef0720405deb143319283cfbc4654c90
LWRGitHub <LoganReynoldsLWR@gmail.com>
2020-11-09 16:21:46 -0800

    Initial Commit

---
+++ calculator-replica/calculator_replicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_replicaApp.swift
+//  calculator-replica
+//
+//  Created by Logan Reynolds on 11/9/20.
+//
+
+import SwiftUI
+
+@main
+struct calculator_replicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
